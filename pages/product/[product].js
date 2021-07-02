28ebb7c36f305ba52f1f72afe2c444e150e769cf
Daniel <drob@megahousecreative.com>
2021-07-01 21:44:48 -0400

    add product detail route
    add product card to product detail page
    add static path and params

--- pages/product/[product].js
+++ pages/product/[product].js
@@ -1,10 +1,41 @@
-import { useRouter } from 'next/router'
+import { useRouter } from "next/router";
+import ProductCard from "../../components/productCard";
 
-const Product = () => {
-  const router = useRouter()
-  const { product } = router.query
-
-  return <p>Product: {product}</p>
+function Product({ product }) {
+  return (
+    <div>
+      <ProductCard
+        key={product.id}
+        detail="true"
+        image={product.image}
+        title={product.title}
+        description={product.description}
+        price={product.price}
+      />
+    </div>
+  );
 }
 
-export default Product
+export async function getStaticPaths() {
+  // Fetch products
+  const res = await fetch("https://fakestoreapi.com/products");
+  const products = await res.json();
+
+  const paths = products.map((product) => ({
+    params: { product: product.id.toString() },
+  }));
+
+  return { paths, fallback: false };
+}
+
+export async function getStaticProps({ params }) {
+  const res = await fetch(
+    `https://fakestoreapi.com/products/${params.product}`
+  );
+  const product = await res.json();
+
+  // Pass post data to the page via props
+  return { props: { product } };
+}
+
+export default Product;
