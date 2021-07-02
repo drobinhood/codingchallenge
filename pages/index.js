e7b17370ece5336a8faa9a2ed65ebc57c923cc1e
Daniel <drob@megahousecreative.com>
2021-07-01 21:38:05 -0400

    add all products to homepage

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,41 @@
 import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
+import React, { useState, useEffect } from "react";
+import ProductCard from "../components/productCard";
 
 export default function Home() {
+  const [products, setProducts] = useState([]);
+  const [loaded, setLoaded] = useState(false);
+
+  useEffect(() => {
+    async function fetchCategories() {
+      try {
+        const res = await fetch("https://fakestoreapi.com/products");
+        const json = await res.json();
+        setProducts(json);
+        setLoaded(true);
+      } catch (err) {
+        console.log("oh gosh, looks like ", err);
+      }
+    }
+
+    fetchCategories();
+  }, []);
+
+  const productList = products.map((product) => {
+    return (
+      <ProductCard
+        key={product.id}
+        id={product.id}
+        image={product.image}
+        title={product.title}
+        description={product.description}
+        price={product.price}
+      />
+    );
+  });
+
   return (
     <>
       <div className={styles.container}>
@@ -16,16 +49,13 @@ export default function Home() {
           <h1 className={styles.title}>
             Welcome to <a href="#">my fake store!</a>
           </h1>
+          <p>Take a look at all our great <i>fake</i> products!</p>
 
-          <div className={styles.grid}>
-          </div>
+          <div className={styles.grid}>{productList}</div>
         </main>
 
         <footer className={styles.footer}>
-          <p
-          >
-            Powered by The Internet
-          </p>
+          <p>Powered by The Internet</p>
         </footer>
       </div>
     </>
