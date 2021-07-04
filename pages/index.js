743b68a478be54b5fc5b0660d0200ae4f0de86dd
Daniel <drob@megahousecreative.com>
2021-07-04 06:58:00 -0400

    adjust global styles
    normally wouldn't be this sloppy with my styles, but I didn't have a plan for styling. I wanted to prioritize functionality first. If I have time I will consolidate classes and layout. Make it look real nice!

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 import Head from "next/head";
-import styles from "../styles/Home.module.css";
+import styles from "../styles/all.module.css";
 import React, { useGlobal} from "reactn";
 import ProductCard from "../components/productCard";
 
