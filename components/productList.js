57d8f06d3f25e1d06cd7d3c199e8ebf70feb461d
Daniel <drob@megahousecreative.com>
2021-07-04 09:18:50 -0400

    update filtering of all products
    add pickers for category and price range
    use static array for price range determination
    use index for price range value
    useEffect to set the product list and ensure uniq when filtering by both price and category
    TODO elevate this functionality for reuse in category pages.

---
+++ components/productList.js
@@ -0,0 +1,16 @@
+import ProductCard from '../components/productCard';
+
+export default function ProductList(props) {
+    return (props.products.map((product) => {
+        return (
+            <ProductCard
+                key={product.id}
+                id={product.id}
+                image={product.image}
+                title={product.title}
+                description={product.description}
+                price={product.price}
+            />
+        );
+    }))
+}
--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,47 @@
 import Head from "next/head";
 import styles from "../styles/all.module.css";
-import React, { useGlobal} from "reactn";
-import ProductCard from "../components/productCard";
+import React, { useGlobal, useState, useEffect } from "reactn";
+import ProductList from "../components/productList";
 
 export default function Home() {
   const [products, setProducts] = useGlobal("products");
+  const [categories, setCategories] = useGlobal("categories");
+  const [sortedProducts, setSortedProducts] = useState([]);
+  const [filterCategory, setFilterCategory] = useState("--");
+  const [filterPriceRange, setFilterPriceRange] = useState(0);
+  const priceRanges = [["--", "--"], [0, 25], [25, 50], [50, 100], [100, 250], [250, 100000000]]
+  //TODO fix useMemo warning
 
-  const productList = products.map((product) => {
-    return (
-      <ProductCard
-        key={product.id}
-        id={product.id}
-        image={product.image}
-        title={product.title}
-        description={product.description}
-        price={product.price}
-      />
-    );
+  useEffect(() => {
+    function onlyUnique(value, index, self) {
+      return self.indexOf(value) === index;
+    }
+    const productList = products
+    const productsByPrice = filterPriceRange !== 0 ?
+      productList.filter(product => {
+        if (product.price >= priceRanges[filterPriceRange][0] && product.price <= priceRanges[filterPriceRange][1]) {
+          return product
+        }
+      })
+      : []
+
+    const productsByCategory = filterCategory !== "" ? productList.filter(product => {
+      if (product.category == filterCategory) {
+        return product
+      }
+    }) : []
+
+    const combined = [...productsByCategory, ...productsByPrice]
+
+    const filtered = combined.filter(onlyUnique)
+
+    setSortedProducts(filtered)
+  }, [filterCategory, filterPriceRange, sortedProducts, setSortedProducts, products, priceRanges ])
+
+  const productList = sortedProducts.length == 0 ? <ProductList products={products}/> : <ProductList products={sortedProducts}/>
+
+  const categoryOptions = categories.map((category) => {
+    return <option value={category} key={category}>{category}</option>
   });
 
   return (
@@ -36,12 +61,27 @@ export default function Home() {
             Take a look at all our great <i>fake</i> products!
           </p>
 
+          <div>
+            <select onChange={e => setFilterPriceRange(e.currentTarget.value)}>
+              <option value={0} default >-- Sort by Price --</option>
+              <option value={1}>$0-25</option>
+              <option value={2}>$25-50</option>
+              <option value={3}>$50-100</option>
+              <option value={4}>$100-250</option>
+              <option value={5}>$250+</option>
+            </select>
+            <select onChange={e => setFilterCategory(e.currentTarget.value)}>
+              <option value="--" default>-- Sort by Category --</option>
+              {categoryOptions}
+            </select>
+          </div>
+
+          {filterCategory || filterPriceRange !== ""
+            ? <p>I&lsquo;m looking for something from {priceRanges[filterPriceRange][0] + " to " + priceRanges[filterPriceRange][1]} in {filterCategory}</p>
+            : ""}
+
           <div className={styles.grid}>
-            {productList.length != 0 ? (
-              productList
-            ) : (
-              <p>...loading some really nice fake products</p>
-            )}
+            {productList}
           </div>
         </main>
 
