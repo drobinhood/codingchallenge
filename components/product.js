5fc2f246329a0567eefb21075daddeaa5235f655
Daniel <drob@megahousecreative.com>
2021-07-05 15:32:42 -0400

    add detail page link to product used in cart

--- components/product.js
+++ components/product.js
@@ -1,21 +1,27 @@
 import Image from "next/image";
 import { useGlobal } from "reactn";
+import Link from "next/link";
 
 export default function Product(props) {
   const [cart, setCart] = useGlobal("cart");
   return (
     <>
       <div className="product">
-        <button
-          onClick={() => {
-            var tempCart = cart
-            const indexToRemove = tempCart.indexOf(props.id.toString())
-            tempCart.splice(indexToRemove,1)
-            setCart([...tempCart])
-          }}
-        >
-          x
-        </button>
+        {props.remove !== false ? (
+          <button
+            className="small reject"
+            onClick={() => {
+              var tempCart = cart;
+              const indexToRemove = tempCart.indexOf(props.id.toString());
+              tempCart.splice(indexToRemove, 1);
+              setCart([...tempCart]);
+            }}
+          >
+            remove
+          </button>
+        ) : (
+          ""
+        )}
         <p className="product-title">{props.title}</p>
         <div className="product-image-container">
           <Image
@@ -28,6 +34,9 @@ export default function Product(props) {
         <div className="product-price">
           <p>{props.price}</p>
         </div>
+        <Link href={"/product/" + props.id} passHref>
+          <button className="small">View Details</button>
+        </Link>
       </div>
     </>
   );
