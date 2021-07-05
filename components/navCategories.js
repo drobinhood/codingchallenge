d93549abef2fd91a2af4443ba51b120b62d93296
Daniel <drob@megahousecreative.com>
2021-07-05 15:34:11 -0400

    clean up declarations

--- components/navCategories.js
+++ components/navCategories.js
@@ -2,7 +2,7 @@ import React, { useGlobal } from "reactn";
 import Link from "next/link";
 
 export default function NavCategories() {
-  const [categories, setCategories] = useGlobal("categories");
+  const [categories] = useGlobal("categories");
 
   const categoryList = categories.map((category) => {
     return <Link key={category} href={`/category/${category}`} passHref><a>{category}</a></Link>;
