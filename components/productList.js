3414f7ba8a9c4934418bd788a7ac44e235a2e6d9
Daniel <drob@megahousecreative.com>
2021-07-05 15:28:51 -0400

    add clarifying comment

--- components/productList.js
+++ components/productList.js
@@ -1,5 +1,8 @@
 import ProductCard from "../components/productCard";
 
+// Returns a list of product cards
+// Requires a array of product objects
+
 export default function ProductList(props) {
   return props.products.map((product) => {
     return (
